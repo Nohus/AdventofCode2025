aece0ad954b0ecad2d48acb579b326577113c368
Nohus <git@nohus.eu>
2025-12-01 06:02:43 +0000

    Day 1

--- src/main/kotlin/day01/Day1_1.kt
+++ src/main/kotlin/day01/Day1_1.kt
@@ -3,5 +3,12 @@ package day01
 import solve
 
 fun main() = solve { lines ->
-
+    var dial = 50
+    lines.count { line ->
+        val direction = if (line[0] == 'R') 1 else -1
+        val amount = line.drop(1).toInt() * direction
+        dial += amount
+        dial %= 100
+        dial == 0
+    }
 }
--- src/main/kotlin/day01/Day1_2.kt
+++ src/main/kotlin/day01/Day1_2.kt
@@ -3,5 +3,16 @@ package day01
 import solve
 
 fun main() = solve { lines ->
-
+    var dial = 50
+    var count = 0
+    lines.forEach { line ->
+        val direction = if (line[0] == 'R') 1 else -1
+        val amount = line.drop(1).toInt()
+        repeat(amount) {
+            dial += direction
+            dial %= 100
+            if (dial == 0) count++
+        }
+    }
+    count
 }
