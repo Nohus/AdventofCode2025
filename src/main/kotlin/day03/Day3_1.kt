b81ee7aebdeed9de713a43fd898332e849318f1a
Nohus <git@nohus.eu>
2025-12-03 12:53:29 +0000

    Day 3

--- src/main/kotlin/day03/Day3_1.kt
+++ src/main/kotlin/day03/Day3_1.kt
@@ -3,5 +3,13 @@ package day03
 import solve
 
 fun main() = solve { lines ->
-
+    lines.sumOf { line ->
+        val nums = mutableListOf<Int>()
+        for (i1 in line.indices) {
+            for (i2 in i1 + 1 until line.length) {
+                nums += "${line[i1]}${line[i2]}".toInt()
+            }
+        }
+        nums.max()
+    }
 }
--- src/main/kotlin/day03/Day3_2.kt
+++ src/main/kotlin/day03/Day3_2.kt
@@ -3,5 +3,18 @@ package day03
 import solve
 
 fun main() = solve { lines ->
-
+    lines.sumOf { line ->
+        var number = ""
+        var lastDigitIndex = -1
+        for (digitIndex in 0 until 12) {
+            val (index, digit) = line
+                .withIndex()
+                .drop(lastDigitIndex + 1)
+                .dropLast(11 - digitIndex)
+                .maxBy { (_, digit) -> digit.digitToInt() }
+            lastDigitIndex = index
+            number += digit
+        }
+        number.toLong()
+    }
 }
