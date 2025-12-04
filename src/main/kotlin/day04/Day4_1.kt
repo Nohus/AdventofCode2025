4a427539910dc84931a0fead669b94f4b3f2361a
Nohus <git@nohus.eu>
2025-12-04 05:15:01 +0000

    Day 4

--- src/main/kotlin/day04/Day4_1.kt
+++ src/main/kotlin/day04/Day4_1.kt
@@ -1,7 +1,11 @@
 package day04
 
 import solve
+import utils.toGrid
 
 fun main() = solve { lines ->
-
+    val grid = lines.toGrid()
+    grid.count { (point, char) ->
+        char == '@' && point.getAdjacent().count { grid[it] == '@' } < 4
+    }
 }
--- src/main/kotlin/day04/Day4_2.kt
+++ src/main/kotlin/day04/Day4_2.kt
@@ -1,7 +1,19 @@
 package day04
 
 import solve
+import utils.toGrid
+import kotlin.collections.component1
+import kotlin.collections.component2
 
 fun main() = solve { lines ->
-
+    val grid = lines.toGrid().toMutableMap()
+    var count = 0
+    while (true) {
+        count += grid.count { (point, char) ->
+            (char == '@' && point.getAdjacent().count { grid[it] == '@' } < 4).also {
+                if (it)  grid[point] = '.'
+            }
+        }.also { if (it == 0) break }
+    }
+    count
 }
