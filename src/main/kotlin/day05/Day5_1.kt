06655bf3b1ddd1a3fc74468987eb991105a4edd5
Nohus <git@nohus.eu>
2025-12-05 06:05:10 +0000

    Day 5

--- src/main/kotlin/day05/Day5_1.kt
+++ src/main/kotlin/day05/Day5_1.kt
@@ -1,7 +1,12 @@
 package day05
 
-import solve
+import solveRaw
 
-fun main() = solve { lines ->
-
+fun main() = solveRaw { input ->
+    val (rangesInput, idsInput) = input.split("\n\n")
+    val ranges = rangesInput.lines().map { line ->
+        line.split("-").let { it[0].toLong()..it[1].toLong() }
+    }
+    val ids = idsInput.lines().map { it.toLong() }
+    ids.count { id -> ranges.any { id in it } }
 }
--- src/main/kotlin/day05/Day5_2.kt
+++ src/main/kotlin/day05/Day5_2.kt
@@ -1,7 +1,23 @@
 package day05
 
-import solve
+import solveRaw
 
-fun main() = solve { lines ->
+fun main() = solveRaw { input ->
+    val rangesToAdd = input.substringBefore("\n\n").lines().map { line ->
+        line.split("-").let { it[0].toLong()..it[1].toLong() }
+    }.toMutableList()
+    val nonOverlappingRanges = mutableListOf<LongRange>()
 
+    outer@while (rangesToAdd.isNotEmpty()) {
+        val new = rangesToAdd.removeFirst()
+        for (existing in nonOverlappingRanges) {
+            if (new.first <= existing.last && new.last >= existing.first) {
+                nonOverlappingRanges -= existing
+                rangesToAdd += minOf(new.first, existing.first)..maxOf(new.last, existing.last)
+                continue@outer
+            }
+        }
+        nonOverlappingRanges += new
+    }
+    nonOverlappingRanges.sumOf { it.last - it.first + 1 }
 }
