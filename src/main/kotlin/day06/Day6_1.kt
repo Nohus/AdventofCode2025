0101aa27b20136335dd65b26f033eea8f4763c25
Nohus <git@nohus.eu>
2025-12-06 05:53:58 +0000

    Day 6

--- src/main/kotlin/day06/Day6_1.kt
+++ src/main/kotlin/day06/Day6_1.kt
@@ -1,7 +1,15 @@
 package day06
 
 import solve
+import utils.findLongs
+import utils.splitWords
 
 fun main() = solve { lines ->
-
+    val rows = lines.dropLast(1).map { it.findLongs() }
+    val columns = List(rows.first().size) { index ->
+        rows.map { it[index] } to lines.last().splitWords()[index]
+    }
+    columns.sumOf { (numbers, operator) ->
+        if (operator == "+") numbers.sum() else numbers.reduce { acc, i -> acc * i }
+    }
 }
--- src/main/kotlin/day06/Day6_2.kt
+++ src/main/kotlin/day06/Day6_2.kt
@@ -2,6 +2,20 @@ package day06
 
 import solve
 
-fun main() = solve { lines ->
-
+fun main() = solve(trim = false) { lines ->
+    val columns = List(lines.maxOf { it.length }) { inputColumn ->
+        lines.map { it.getOrNull(inputColumn) ?: "" }.joinToString("").trim()
+    } + listOf("")
+    val problem = mutableListOf<String>()
+    columns.sumOf { column ->
+        if (column.isNotBlank()) {
+            problem += column
+            0
+        } else {
+            val operator = problem.first().last()
+            val numbers = problem.drop(1).map { it.toLong() } + problem.first().dropLast(1).trim().toLong()
+            problem.clear()
+            if (operator == '+') numbers.sum() else numbers.reduce { acc, i -> acc * i }
+        }
+    }
 }
