929a0c44f3829399354e033673ff48994a9a12f6
Nohus <git@nohus.eu>
2025-12-07 05:44:55 +0000

    Day 7

--- src/main/kotlin/day07/Day7_1.kt
+++ src/main/kotlin/day07/Day7_1.kt
@@ -1,7 +1,31 @@
 package day07
 
 import solve
+import utils.Direction.EAST
+import utils.Direction.SOUTH
+import utils.Direction.WEST
+import utils.toGrid
 
 fun main() = solve { lines ->
+    val grid = lines.toGrid()
+    val start = grid.entries.first { (_, char) -> char == 'S' }.key
+    val beams = mutableSetOf(start)
+    var count = 0
 
+    while (beams.isNotEmpty()) {
+        beams.toList().forEach { beam ->
+            beams -= beam
+            val next = beam.move(SOUTH)
+            when (grid[next]) {
+                '.' -> beams += next
+                '^' -> {
+                    count++
+                    beams += next.move(WEST)
+                    beams += next.move(EAST)
+                }
+            }
+        }
+    }
+
+    count
 }
--- src/main/kotlin/day07/Day7_2.kt
+++ src/main/kotlin/day07/Day7_2.kt
@@ -1,7 +1,25 @@
 package day07
 
 import solve
+import utils.Direction.EAST
+import utils.Direction.SOUTH
+import utils.Direction.WEST
+import utils.Point
+import utils.toGrid
 
 fun main() = solve { lines ->
+    val grid = lines.toGrid()
+    val cache = mutableMapOf<Point, Long>()
 
+    fun count(beam: Point): Long {
+        cache[beam]?.let { return it }
+        val next = beam.move(SOUTH)
+        return when (grid[next]) {
+            '.' -> count(next)
+            '^' -> count(next.move(WEST)) + count(next.move(EAST))
+            else -> 1
+        }.also { cache[beam] = it }
+    }
+
+    count(grid.entries.first { (_, char) -> char == 'S' }.key)
 }
