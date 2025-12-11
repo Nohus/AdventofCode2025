0b3ca885fd85085d36ddb6caeddc92dfdf1d8ed2
Nohus <git@nohus.eu>
2025-12-11 05:23:50 +0000

    Day 11

--- src/main/kotlin/day11/Day11_1.kt
+++ src/main/kotlin/day11/Day11_1.kt
@@ -3,5 +3,15 @@ package day11
 import solve
 
 fun main() = solve { lines ->
+    val outputs = lines.map {
+        it.substringBefore(": ") to it.substringAfter(": ").split(" ")
+    }.toMap()
 
+    fun count(device: String): Int {
+        return outputs[device]!!.sumOf { output ->
+            if (output == "out") 1 else count(output)
+        }
+    }
+
+    count("you")
 }
--- src/main/kotlin/day11/Day11_2.kt
+++ src/main/kotlin/day11/Day11_2.kt
@@ -3,5 +3,19 @@ package day11
 import solve
 
 fun main() = solve { lines ->
+    val outputs = lines.map {
+        it.substringBefore(": ") to it.substringAfter(": ").split(" ")
+    }.toMap()
 
+    data class Key(val device: String, val hasFft: Boolean, val hasDac: Boolean)
+    val cache = mutableMapOf<Key, Long>()
+    fun count(key: Key): Long {
+        cache[key]?.let { return it }
+        return outputs[key.device]!!.sumOf { output ->
+            if (output == "out") if (key.hasFft && key.hasDac) 1 else 0
+            else count(Key(output, key.hasFft || output == "fft", key.hasDac || output == "dac"))
+        }.also { cache[key] = it }
+    }
+
+    count(Key("svr", false, false))
 }
